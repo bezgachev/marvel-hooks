f6229295f4d8ff73c7a397b3e3b277df120fd89e
Ivan Bezgachev <i.bezgachev@yandex.ru>
2024-04-18 14:32:31 +0300

    маршрутизация router 5v, отдельные страницы

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from "react-dom/client";
 import App from './components/app/App';
-// import MarvelService from './services/MarvelService';
 import './style/style.scss';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
     // eslint-disable-next-line
-    // <StrictMode>
+    <StrictMode>
         <App />
-    // eslint-disable-next-line
-    // </StrictMode>
+    </StrictMode>
 );
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -23,8 +23,8 @@ const useMarvelService = () => {
     }
 
     const getComic = async (id) => {
-		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
-		return _transformComics(res.data.results[0]);
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
 	}
 
     const _transformCharacter = (char) => {
